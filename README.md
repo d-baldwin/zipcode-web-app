f4b2af0bc35dc3fb93143938843ce35cee19e2fa
dbaldwin <baldwin.derrick@gmail.com>
2017-04-01 15:09:39 -0400

    test comnecting to cloud database

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# zipcode-web-app
+## zipcode-web-app
+
+### Must set up the target datatbase as Environment Variable. 
+
+`
+export MLAB_URL="mongodb://\<username>:\<password>@ds055574.mlab.com:55574/zips_production"
+`
+
+
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+
+	"os"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+)
+
+
+type Zip struct {
+	ID string `json:"_id"`
+	City string `json:"city"`
+	Loc []float64 `json:"loc"`
+	Pop int `json:"pop"`
+	State string `json:"state"`
+}
+
+
+func main() {
+
+	// connection string
+	uri := os.Getenv("MLAB_URI")
+	if uri == "" {
+		fmt.Println("mlab.com environment variable not working")
+		os.Exit(1)
+	}
+
+	// start session
+	session, err := mgo.Dial(uri)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// zips collection
+	c := session.DB("zips_production").C("zips")
+
+
+	// What is the population with San Francisco zipcodes
+	var result []Zip
+	err = c.Find(bson.M{"city": "SAN FRANCISCO"}).Select(bson.M{"pop": 1}).All(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var cityPopulation = 0
+	for _,code := range result {
+		cityPopulation += code.Pop
+	}
+
+	fmt.Printf("San Francisco zip code population is %v\n", cityPopulation)
+
+
+
+}
